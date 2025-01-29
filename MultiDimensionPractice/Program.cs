1eb6cf879fcab7c4d2baad3e6a97ed8daf07898c
Shubh Shrimali <shubh.shrimali2002@gmail.com>
2025-01-29 10:49:14 +0530

    any msg

---
+++ MultiDimensionPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Rows :");
+        int rows = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Columns :");
+        int cols = int.Parse(Console.ReadLine());
+
+        int[,] mtrx = new int[rows, cols];
+    }
+}
